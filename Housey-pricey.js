6c4f18c46f62046282a150ef802ef32824c41672
DPASCII <davidcpascual@gmail.com>
2023-02-18 13:30:28 +0800

    Fixed Housey-pricey

--- Housey-pricey.js
+++ Housey-pricey.js
@@ -1,13 +1,18 @@
+"use strict";
 let volumeInMeters = 8 * 10 * 10;
 let gardenSizeInM2 = 100;
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+let housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 const housePeter = 2500000;
+let priceComparison = housePeter > housePrice ? `over` : `under`;
+let buyer = "Peter";
 
-console.log("is Peter paying too much?", housePeter > housePrice);
+console.log(`${buyer} is ${priceComparison}paying for the house`);
 
 volumeInMeters = 5 * 11 * 8;
 gardenSizeInM2 = 70;
 housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 const houseJulia = 1000000;
+priceComparison = houseJulia > housePrice ? `over` : `under`;
+buyer = "Julia";
 
-console.log("is Julia paying too much?", houseJulia > housePrice);
+console.log(`${buyer} is ${priceComparison}paying for the house`);
