c4640335e9bf178ebbbf53db41640ccc301c0440
DPASCII <davidcpascual@gmail.com>
2023-02-18 13:30:21 +0800

    Fixed Goodboy-Oldboy

--- Goodboy-Oldboy.js
+++ Goodboy-Oldboy.js
@@ -1,32 +1,11 @@
+"use strict";
 const dogYearOfBirth = 2018;
-const dogYear = 7;
-let dogYearFuture = 2025;
-let shouldShowResultInDogYears = -1;
-let humanYears =
-  "Your dog will be " +
-  (dogYearFuture - dogYearOfBirth) +
-  " human years old in " +
-  dogYearFuture;
-let dogYears =
-  "Your dog will be " +
-  (dogYearFuture - dogYearOfBirth) * 7 +
-  " dog years old in " +
-  dogYearFuture;
+const dogYearFuture = 2025;
+const shouldShowResultInDogYears = false;
+const humanYears = dogYearFuture - dogYearOfBirth;
+const type = shouldShowResultInDogYears ? `dog` : `human`;
+const ageDisplay = shouldShowResultInDogYears ? humanYears * 7 : humanYears;
 
-console.log((shouldShowResultInDogYears && dogYears) || humanYears);
-
-// if (shouldShowResultInDogYears === 1) {
-//   console.log(
-//     "Your dog will be",
-//     dogYearFuture - dogYearOfBirth,
-//     "human years old in",
-//     dogYearFuture
-//   );
-// } else {
-//   console.log(
-//     "Your dog will be",
-//     (dogYearFuture - dogYearOfBirth) * 7,
-//     "dog years old in",
-//     dogYearFuture
-//   );
-// }
+console.log(
+  `Your dog will be ${ageDisplay} ${type} years old in ${dogYearFuture}`
+);
