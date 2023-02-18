25ce26b9ee84952b24423a9b2c2c85e6c797db0f
DPASCII <davidcpascual@gmail.com>
2023-02-18 13:30:01 +0800

    Fixed Age-ify

--- Age-ify.js
+++ Age-ify.js
@@ -1,9 +1,7 @@
+"use strict";
 const yearOfBirth = 1994;
-
 const yearFuture = 2050;
 const age = yearFuture - yearOfBirth;
-
-console.log("You will be " + age + " years old in " + yearFuture);
-
-// alternatively
 const str = `You will be ${age} years old in ${yearFuture}`;
+
+console.log(str);
