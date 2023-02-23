117b6d8febcfc260d87295d2d237e80393ada4ca
DPASCII <davidcpascual@gmail.com>
2023-02-23 22:07:33 +0800

    Redid code to our rules for simplification

--- Week-2/4.Student-manager.js
+++ Week-2/4.Student-manager.js
@@ -1,30 +1,30 @@
 "use strict";
 
 const class07Students = [];
-function addStudentToClass(studentName) {
-  if (studentName.trim() != "") {
-    if (
-      class07Students.find(
-        function (class07Students) {
-          return class07Students === studentName;
-        }
-        //(class07Students) => class07Students === studentName (Found this can pls explain haha)
-      ) != studentName
-    ) {
-      if (studentName == "Queen") {
-        class07Students.push(studentName.trim());
-      } else if (class07Students.length <= 5) {
-        class07Students.push(studentName.trim());
-      } else {
-        console.log("Cannot add more students to class 07");
-      }
-    } else {
-      console.log(`Student ${studentName.trim()} is already in the class`);
-    }
-  } else {
+
+function addStudentToClass(name) {
+  const studentName = name.trim();
+  // invalid inputs
+  if (!studentName) {
     console.log("Invalid Input");
+    return;
   }
+
+  // duplicate students
+  if (class07Students.indexOf(studentName) >= 0) {
+    console.log(`Student ${studentName} is already in the class`);
+    return;
+  }
+
+  //if class if full
+  if (class07Students.length > 6 && studentName != "queen") {
+    console.log("Cannot add more students to class 07");
+    return;
+  }
+
+  class07Students.push(studentName);
 }
+
 function getNumberOfStudents() {
   return class07Students.length;
 }
@@ -38,6 +38,7 @@ addStudentToClass("Kai");
 addStudentToClass("Gabriel");
 addStudentToClass("Queen");
 addStudentToClass("");
+addStudentToClass("Darresn");
 
 console.log(class07Students);
 console.log(getNumberOfStudents());
