e203fa2f21d257cff594b9837cdcb661fb6f011b
DPASCII <davidcpascual@gmail.com>
2023-03-06 16:31:52 +0800

    ran the code made necessary corrections such and included final output into return value

--- Week-4/Lesson-Plan/CreditCardFormat.js
+++ Week-4/Lesson-Plan/CreditCardFormat.js
@@ -1,26 +1,25 @@
 function formatCreditCardNumber(x) {
   //if x is not a number
-  if (x != "number") {
+  if (typeof x != "number") {
     console.log("error");
     return;
   }
 
-  //store integer into object
-  const output = { original: x, formatted };
-
   //turn integer into string
   const number = x.toString();
 
-  //format and the string
-  output.formatted =
-    number.split("", 4) +
-    " " +
-    str.slice(4, 8) +
-    " " +
-    str.slice(8, 12) +
-    " " +
-    str.slice(12);
-
   //return object
-  return output;
+  return {
+    original: x,
+    formatted:
+      number.slice(0, 4) +
+      " " +
+      number.slice(4, 8) +
+      " " +
+      number.slice(8, 12) +
+      " " +
+      number.slice(12),
+  };
 }
+
+console.log(formatCreditCardNumber(1111222233334444));
